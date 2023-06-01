08277abe027b6d31bd59380f906043738998078f
EdvardEgorov <135224482+EdvardEgorov@users.noreply.github.com>
2023-06-01 18:02:34 +0300

    Add files via upload

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+if (a > (b+c))
+    Console.WriteLine(a);
+else if (b > (a+c))
+    Console.WriteLine(b);
+else if (c > (a+b))
+    Console.WriteLine(c);
+
+   
+
+    
---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int remDiv = n % 2;
+if (remDiv == 0)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
+
+
+  
+
